5933f140665f946cdbaecfd8fc0e0ddcf9aa22b8
juncyan <125954099+juncyan@users.noreply.github.com>
2025-10-03 13:56:14 +0800

    Update bcd_trainer.py

--- bcd_trainer.py
+++ bcd_trainer.py
@@ -7,20 +7,12 @@ import argparse
 from functools import partial
 
 
-from cd_models.fccdn import FCCDN
-from cd_models.stanet import STANet
-from cd_models.p2v import P2V
 from cd_models.msfgnet import MSFGNet
 from cd_models.fc_siam_conc import FCSiamConc
-from cd_models.dsamnet import DSAMNet
-from cd_models.snunet import SNUNet
 from cd_models.f3net import F3Net
-from paddleseg.models import UNet
 from cd_models.replkcd import CD_RLKNet
 from cd_models.efisam import EFISam
-from paddleseg.models import UNet
-from cd_models.cienet.ciescd import CIENetTinyViT
-from scfnet.model import SCFNet_BCD
+from filora.ffinet_tinyvit import FFINetVT_bCD
 
 from core.bcdwork import Work
 
@@ -36,17 +28,6 @@ dataset_name = "WHU_BCD"
 
 dataset_path = '/mnt/data/Datasets/{}'.format(dataset_name)
 
-# res = ResNet50_vd()
-# model = UNet(num_classes, in_channels=6)
-# model = UNetPlusPlus(num_classes, 6)
-# model = UPerNet(num_classes, ResNet50_vd(in_channels=6),(0,1,2,3))
-# model = DeepLabV3P(num_classes, backbone=ResNet50_vd(in_channels=6))
-# model = SegNeXt(num_classes=num_classes, decoder_cfg={}, backbone=ResNet50_vd(in_channels=6))
-# model = DSAMNet(3,2)
-# model = MSFGNet(3, 2)
-# model = P2V(3,2)
-# model = FCCDN(3,2)
-# model = FCSiamConc(3,2)
 
 pil_logger = logging.getLogger('PIL')
 pil_logger.setLevel(logging.INFO)
@@ -98,9 +79,7 @@ if __name__ == "__main__":
     # model = CIENetTinyViT(img_size=args.img_size)
     # model = CD_RLKNet()
     # model = EFISam(img_size=args.img_size)
-    # model = FFINetVT_CD(img_size=args.img_size)
-    model = SCFNet_BCD(img_size=args.img_size)
-   
+    model = FFINetVT_CD(img_size=args.img_size)
     w = Work(model, args)
   
 
