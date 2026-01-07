fcdd1e203702a2c83d1b2a69a5bcd26977dcfc2a
juncyan <125954099+juncyan@users.noreply.github.com>
2026-01-07 22:01:31 +0800

    fix mobile sam file

--- segment_anything/modeling/__init__.py
+++ segment_anything/modeling/__init__.py
@@ -20,4 +20,4 @@ from .image_encoder import ImageEncoderViT
 from .mask_decoder import MaskDecoder
 from .prompt_encoder import PromptEncoder
 from .transformer import TwoWayTransformer
-from .ad_tiny_vit import TinyViT
+from .adtinyvit import TinyViT
